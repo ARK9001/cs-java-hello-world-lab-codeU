545db18dc4b460277c90bf82a1879f29b1a55994
ARK9001 <aish2012@gmail.com>
2016-03-31 14:30:49 -0400

    CodeU Java HelloWorld Submission

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -3,9 +3,13 @@ package com.flatironschool.javacs;
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
+        String javaVerS = System.getProperty("java.specification.version");
+        Double javaVerD = Double.parseDouble(javaVerS);
+        return javaVerD;
     }
 
     public static void main(String[] args) {
 	    // you can test the output of getVersion() here
+	    System.out.println(getVersion());
     }
 }
